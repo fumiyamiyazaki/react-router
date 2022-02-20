eaf72805832b594e5dfd9f878d7a6785439042ba
fumiyamiyazaki <57476802+fumiyamiyazaki@users.noreply.github.com>
2022-02-21 00:28:40 +0900

    separate route

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,19 +2,22 @@ import { Route, Routes } from "react-router-dom";
 
 import { Home } from "../Home";
 import { Page1 } from "../Page1";
-import { Page1DetailA } from "../Page1DetailA";
-import { Page1DetailB } from "../Page1DetailB";
-import { Page1Index } from "../Page1Index";
 import { Page2 } from "../Page2";
+import { page1Routes } from "./Page1Routes";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/page1/*" element={<Page1 />}>
-        <Route index element={<Page1Index />} />
-        <Route path="DetailA" element={<Page1DetailA />} />
-        <Route path="DetailB" element={<Page1DetailB />} />
+        {page1Routes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            index={route.index}
+            element={route.children}
+          />
+        ))}
       </Route>
       <Route path="/page2/*" element={<Page2 />} />
     </Routes>
